9759dccae24c6e90bac0a715ff6e3d262da0150a
tvolf <gtvolf@gmail.com>
2025-06-25 16:42:47 +0300

    Updated api.php

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,16 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [ RegisterController::class, 'register'])->name('auth.register')->middleware('guest');
 Route::post('login', [ LoginController::class, 'login'])->name('auth.login')->middleware('guest');
+
+Route::apiResource('posts', PostController::class)->only(['index', 'show']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::apiResource('posts', PostController::class)->only(['store', 'update', 'destroy']);
+});
 
 
 //Route::get('/user', function (Request $request) {
